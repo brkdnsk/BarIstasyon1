5cc9e78f87940e716250bce850c49d2219e2f89c
denizzylcn <116671202+meryemorc@users.noreply.github.com>
2025-05-19 19:57:08 +0300

    AverageRatingDto

---
+++ Baristasyon.Application/Dtos/AverageRatingDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baristasyon.Application.Dtos
+{
+    public class AverageRatingDto
+    {
+        public double Average { get; set; }
+        public int Count { get; set; }
+    }
+}
