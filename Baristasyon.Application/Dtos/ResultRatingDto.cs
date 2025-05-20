8eba5653f1110a6d8947d463f006fb60cbe969f5
brkdnsk <bburakdnsk@gmail.com>
2025-05-20 05:08:13 +0300

    Admin

---
+++ Baristasyon.Application/Dtos/ResultRatingDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baristasyon.Application.Dtos
+{
+    public class ResultRatingDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CoffeeRecipeId { get; set; }
+        public int Score { get; set; }
+        public DateTime RatedAt { get; set; }
+
+        public string Username { get; set; } = null!;
+        public string RecipeTitle { get; set; } = null!;
+    }
+}
--- Baristasyon.WebUI/Controllers/Admin/AdminRatingController.cs
+++ Baristasyon.WebUI/Controllers/Admin/AdminRatingController.cs
@@ -4,11 +4,11 @@ using Newtonsoft.Json;
 
 namespace Baristasyon.WebUI.Controllers.Admin
 {
-    public class AdminReviewController : Controller
+    public class AdminRatingController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public AdminReviewController(IHttpClientFactory httpClientFactory)
+        public AdminRatingController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -16,31 +16,22 @@ namespace Baristasyon.WebUI.Controllers.Admin
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient("api");
-            var response = await client.GetAsync("review");
+            var response = await client.GetAsync("rating");
 
             if (!response.IsSuccessStatusCode)
-                return View(new List<ResultReviewDto>());
+                return View(new List<ResultRatingDto>());
 
             var json = await response.Content.ReadAsStringAsync();
-            var data = JsonConvert.DeserializeObject<List<ResultReviewDto>>(json);
+            var data = JsonConvert.DeserializeObject<List<ResultRatingDto>>(json);
             return View(data);
         }
 
-       
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
         {
             var client = _httpClientFactory.CreateClient("api");
-
-            var response = await client.DeleteAsync($"review/{id}");
-
-            if (!response.IsSuccessStatusCode)
-            {
-                TempData["Error"] = "Yorum silinirken bir hata oluştu.";
-            }
-
+            await client.DeleteAsync($"rating/{id}");
             return RedirectToAction("Index");
         }
-
     }
 }
